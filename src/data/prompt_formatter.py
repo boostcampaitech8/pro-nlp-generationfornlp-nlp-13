b2f68c45d9b9461c742c44df95cc9f463f0f6a47
jis-arhcive <gkswltjr00@gmail.com>
2025-12-24 13:00:43 +0000

    CHORE: edit files

--- src/data/prompt_formatter.py
+++ src/data/prompt_formatter.py
@@ -4,31 +4,31 @@ class PromptFormatter:
     """프롬프트 템플릿 생성"""
     
     PROMPT_NO_QUESTION_PLUS = """지문:
-{paragraph}
+    {paragraph}
 
-질문:
-{question}
+    질문:
+    {question}
 
-선택지:
-{choices}
+    선택지:
+    {choices}
 
-1, 2, 3, 4, 5 중에 하나를 정답으로 고르세요.
-정답:"""
+    1, 2, 3, 4, 5 중에 하나를 정답으로 고르세요.
+    정답:"""
 
     PROMPT_QUESTION_PLUS = """지문:
-{paragraph}
+    {paragraph}
 
-질문:
-{question}
+    질문:
+    {question}
 
-<보기>:
-{question_plus}
+    <보기>:
+    {question_plus}
 
-선택지:
-{choices}
+    선택지:
+    {choices}
 
-1, 2, 3, 4, 5 중에 하나를 정답으로 고르세요.
-정답:"""
+    1, 2, 3, 4, 5 중에 하나를 정답으로 고르세요.
+    정답:"""
     
     @staticmethod
     def format_choices(choices: List[str]) -> str:
--- src/training/metrics.py
+++ src/training/metrics.py
@@ -2,34 +2,47 @@ import numpy as np
 import torch
 import evaluate
 
-# 모델의 logits 를 조정하여 정답 토큰 부분만 출력하도록 설정
-def preprocess_logits_for_metrics(logits, labels, tokenizer):
-    logits = logits if not isinstance(logits, tuple) else logits[0]
-    logit_idx = [tokenizer.vocab["1"], tokenizer.vocab["2"], tokenizer.vocab["3"], tokenizer.vocab["4"], tokenizer.vocab["5"]]
-    logits = logits[:, -2, logit_idx] # -2: answer token, -1: eos token
-    return logits
+# preprocess_logits_for_metrics 함수를 반환하는 팩토리 함수
+def get_preprocess_logits_for_metrics(tokenizer):
+    """
+    tokenizer를 클로저로 캡처하여 preprocess_logits_for_metrics 함수를 반환
+    """
+    def preprocess_logits_for_metrics(logits, labels):
+        logits = logits if not isinstance(logits, tuple) else logits[0]
+        logit_idx = [tokenizer.vocab["1"], tokenizer.vocab["2"], tokenizer.vocab["3"], tokenizer.vocab["4"], tokenizer.vocab["5"]]
+        logits = logits[:, -2, logit_idx] # -2: answer token, -1: eos token
+        return logits
+    
+    return preprocess_logits_for_metrics
 
-# metric 계산 함수
-def compute_metrics(evaluation_result, tokenizer):
 
-    # metric 로드
-    acc_metric = evaluate.load("accuracy")
-
+# metric 계산 함수를 반환하는 팩토리 함수
+def get_compute_metrics(tokenizer):
+    """
+    tokenizer를 클로저로 캡처하여 compute_metrics 함수를 반환
+    """
+    # metric 로드 (F1-score로 변경)
+    f1_metric = evaluate.load("f1")
+    
     # 정답 토큰 매핑
     int_output_map = {"1": 0, "2": 1, "3": 2, "4": 3, "5": 4}
+    
+    def compute_metrics(evaluation_result):
+        logits, labels = evaluation_result
 
-    logits, labels = evaluation_result
+        # 토큰화된 레이블 디코딩
+        labels = np.where(labels != -100, labels, tokenizer.pad_token_id)
+        labels = tokenizer.batch_decode(labels, skip_special_tokens=True)
+        labels = list(map(lambda x: x.split("<end_of_turn>")[0].strip(), labels))
+        labels = list(map(lambda x: int_output_map[x], labels))
 
-    # 토큰화된 레이블 디코딩
-    labels = np.where(labels != -100, labels, tokenizer.pad_token_id)
-    labels = tokenizer.batch_decode(labels, skip_special_tokens=True)
-    labels = list(map(lambda x: x.split("<end_of_turn>")[0].strip(), labels))
-    labels = list(map(lambda x: int_output_map[x], labels))
+        # 소프트맥스 함수를 사용하여 로그트 변환
+        probs = torch.nn.functional.softmax(torch.tensor(logits), dim=-1)
+        predictions = np.argmax(probs, axis=-1)
 
-    # 소프트맥스 함수를 사용하여 로그트 변환
-    probs = torch.nn.functional.softmax(torch.tensor(logits), dim=-1)
-    predictions = np.argmax(probs, axis=-1)
-
-    # 정확도 계산
-    acc = acc_metric.compute(predictions=predictions, references=labels)
-    return acc
+        # F1-score 계산 (average='macro'로 설정하여 macro F1-score 계산)
+        f1 = f1_metric.compute(predictions=predictions, references=labels, average='macro')
+        
+        return f1
+    
+    return compute_metrics
